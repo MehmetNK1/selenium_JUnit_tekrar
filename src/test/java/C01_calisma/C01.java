bcc9324ac7cd56ca9cb144cf9fe60da9dc78710b
Mehmet Nail Kocagoz <mehmetnailkocagoz@gmail.com>
2022-05-11 17:10:18 +0200

    11/05

---
+++ src/test/java/C01_calisma/C01.java
@@ -0,0 +1,40 @@
+package C01_calisma;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class C01 {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        //driver.get("https://www.amazon.com");
+
+        driver.get("https://demoqa.com/text-box");
+        WebElement fullName = driver.findElement(By.id("userName"));
+        fullName.click();
+        fullName.sendKeys("mehmet");
+
+        WebElement email = driver.findElement(new By.ByCssSelector(".mr-sm-2[id=\"userEmail\"]"));
+        email.click();
+        email.sendKeys("mehmet123@gmail.com");
+
+        WebElement currentAddress = driver.findElement(By.id("currentAddress"));
+        currentAddress.click();
+        currentAddress.sendKeys("leipziger str 123");
+
+        WebElement permanentAddress = driver.findElement(By.id("permanentAddress"));
+        permanentAddress.click();
+        permanentAddress.sendKeys("bornova / 08058");
+
+        WebElement submitButton = driver.findElement(new By.ByCssSelector("button.btn"));
+        submitButton.click();
+
+
+
+    }
+}
